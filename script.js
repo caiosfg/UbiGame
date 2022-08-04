813aeb90d9441773fb6146e7467f1fd65befc4bd
caiosfg <caio.sfg@gmail.com>
2022-08-03 23:48:20 -0300

    alguns testes

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
-
+// 8:51
 const playerImage = new Image();
-playerImage.src = 'gentleman.png'
-let x = 0;
+playerImage.src = 'shadow_dog.png'
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.fillRect(x, 50, 100, 100);
-    x++;
+    ctx.fillRect(50, 50, 100, 100);
+    ctx.drawImage(playerImage, 50, 50);
     requestAnimationFrame(animate);
 };
 animate();
