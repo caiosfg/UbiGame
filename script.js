a405460ce657846ff8414de0abb5144b4b5ea025
caiosfg <caio.sfg@gmail.com>
2022-08-11 22:10:48 -0300

    some moves

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
-//21:47
+//37:19
 const playerImage = new Image();
 playerImage.src = 'shadow_dog.png'
 const spriteWidth = 575;
@@ -12,14 +12,40 @@ let frameY = 0;
 let gameFrame = 0;
 const staggerFrames = 2;
 
+const spriteAnimations = [];
+
+const animationStates = [
+    {
+        name: 'idle',
+        frames: 7
+    },
+    {
+        name: 'jump',
+        frames: 7
+    }
+];
+
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: [],
+    }
+
+    for (let j = 0; j < state.frames; j++){
+        let positionX = j * spriteWidth;
+        let positionY = index * spriteHeight;
+
+        frames.loc.push({x: positionX, y: positionY})
+    }
+    spriteAnimations[state.name] = frames;
+})
+
+console.log(spriteAnimations)
+
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    // ctx.fillRect(100, 50, 100, 100);
-    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
-    if(gameFrame % staggerFrames == 0) {
-        if(frameX < 6) frameX++;
-        else frameX = 0;
-    }
+    let position = Math.floor(gameFrame /staggerFrames) % spriteAnimations["idle"].loc.length;
+    frameX = spriteWidth * position;
+    ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
 
     gameFrame++;
     requestAnimationFrame(animate);
