4d1e076cba35165afd5be44b3c5822f342441115
caiosfg <caio.sfg@gmail.com>
2022-08-19 23:32:05 -0300

    adicionando alguns controles

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+let playerState = 'fall';
+const dropdown = document.getElementById('animations');
+dropdown.addEventListener('change', function(e){
+    playerState =  e.target.value;
+})
+
+
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
-//37:19
+//42:40
 const playerImage = new Image();
 playerImage.src = 'shadow_dog.png'
 const spriteWidth = 575;
 const spriteHeight = 523;
-let frameX = 0;
-let frameY = 0;
+
 let gameFrame = 0;
 const staggerFrames = 2;
 
@@ -22,7 +28,39 @@ const animationStates = [
     {
         name: 'jump',
         frames: 7
-    }
+    },
+    {
+        name: 'fall',
+        frames: 7
+    },
+    {
+        name: 'run',
+        frames: 9
+    },
+    {
+        name: 'dizzy',
+        frames: 11
+    },
+    {
+        name: 'sit',
+        frames: 5
+    },
+    {
+        name: 'roll',
+        frames: 7
+    },
+    {
+        name: 'bite',
+        frames: 7
+    },
+    {
+        name: 'ko',
+        frames: 12
+    },
+    {
+        name: 'getHit',
+        frames: 4
+    },
 ];
 
 animationStates.forEach((state, index) => {
@@ -43,9 +81,11 @@ console.log(spriteAnimations)
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    let position = Math.floor(gameFrame /staggerFrames) % spriteAnimations["idle"].loc.length;
-    frameX = spriteWidth * position;
-    ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    let position = Math.floor(gameFrame /staggerFrames) % spriteAnimations[playerState].loc.length;
+    let frameX = spriteWidth * position;
+    let frameY = spriteAnimations[playerState].loc[position].y;
+
+    ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
 
     gameFrame++;
     requestAnimationFrame(animate);
